8702e91ed34e93cd8cd2147a4ab77452ca72346b
j-ampulski <A3028650@southstaffs.ac.uk>
2026-04-20 12:03:16 +0100

    minor changes to enemies

--- Assets/Scripts/Enemy/EnemyMovement.cs
+++ Assets/Scripts/Enemy/EnemyMovement.cs
@@ -21,12 +21,6 @@ public class EnemyMovement : MonoBehaviour
         _playerAwarnessController = GetComponent<PlayerAwarenessControler>();
     }
 
-    // Update is called once per frame
-    void Update()
-    {
-
-    }
-
     private void FixedUpdate()
     {
         UpdateTargetDirection();
