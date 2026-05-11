a3ea8e1a5882779f70209a4ea0eb7a452069f333
j-ampulski <A3028650@southstaffs.ac.uk>
2026-05-11 11:39:12 +0100

    comments

--- Assets/Scripts/EndGame.cs
+++ Assets/Scripts/EndGame.cs
@@ -10,12 +10,12 @@ public class EndGameManager : MonoBehaviour
     [SerializeField] 
     private TextMeshProUGUI ScoreText;
 
-    private void Start()
+    private void Start()  // making sure the end screen isnt accidently on
     {
         endScreen.SetActive(false);
     }
 
-    private void OnTriggerEnter2D(Collider2D other)
+    private void OnTriggerEnter2D(Collider2D other)   // checking if the collision was the player and if it was then show end screen
     {
         if (other.CompareTag("Player"))
         {
@@ -23,25 +23,25 @@ public class EndGameManager : MonoBehaviour
         }
     }
 
-    void ShowEndScreen()
+    void ShowEndScreen()  
     {
-        endScreen.SetActive(true);
+        endScreen.SetActive(true); // Turning on the endscreen so the player can see it
 
-        if (CurrencyManager.Instance != null)
+        if (CurrencyManager.Instance != null)  // Making sure that i have a score system
         {
-            ScoreText.text = "Score: " + CurrencyManager.Instance.money;
+            ScoreText.text = "Score: " + CurrencyManager.Instance.money; // Showing what score the player has accumulated
         }
         else
         {
-            ScoreText.text = "Score: 0";
+            ScoreText.text = "Score: 0"; // Keeping the score as 0 if no currecny manager script
         }
 
-        Time.timeScale = 0f;
+        Time.timeScale = 0f;  // Also freezes the  game so the player can do anything but only move his mouse
     }
 
     public void Respawn()
     {
-        Time.timeScale = 1f;
-        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex);
+        Time.timeScale = 1f;  // Un freezes the game if the player has respawned
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex); // Where to respawn the player
     }
 }
